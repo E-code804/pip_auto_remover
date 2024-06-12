a1cbeb16a5407a6d09a1607c59a90e9e80edbdab
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2024-06-11 21:45:23 -0400

    Ability to read each line of the downloaded pip packages text file and output the required-by section.

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ import os
 
 # Outputs the number of packages required by
 pkg = "numpy"
-print(len(pkg_resources.get_distribution(pkg).requires()))
+'''print(len(pkg_resources.get_distribution(pkg).requires()))
 
 # Create a txt file with all downloaded packages.
 os.system("pip list > pip_packages.txt")
 # Shows details about a pkg
-os.system(f"pip show {pkg}")
+os.system(f"pip show {pkg}")'''
 
 # Storing the result of a command.
-command = "echo Hello, World!"
-os.system(command)
+command = f"pip show {pkg}"
+# os.system(command)
 # Capture the output of the command using subprocess.
-result = os.popen(command).read() # Result is now a string.
-print("Output from command:", result[0])
+result = os.popen(command).read()  # Result is now a string.
+print("Output from command:", result.split("\n")[-2])
+
+# Reading all the lines from the file with all downloaded packages.
+with open("pip_packages.txt", "r") as f:
+    packages = f.readlines()[2:]
+
+# Display each package.
+for package in packages:
+    first_space = package.index(' ')
+    curr_package = package[: first_space]
+    result = os.popen(command).read()  # Result is now a string.
+    print(curr_package)
+    print("Output from command:", result.split("\n")[-2])
