49f97d958d0587e78ddd6630c84b436d53ee9a45
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2024-06-16 19:56:33 -0400

    Ability to list all packages that are not required by other complete.

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 import pkg_resources
 import os
 
-# Outputs the number of packages required by
-pkg = "numpy"
-'''print(len(pkg_resources.get_distribution(pkg).requires()))
-
 # Create a txt file with all downloaded packages.
 os.system("pip list > pip_packages.txt")
-# Shows details about a pkg
-os.system(f"pip show {pkg}")'''
-
-# Storing the result of a command.
-command = f"pip show {pkg}"
-# os.system(command)
-# Capture the output of the command using subprocess.
-result = os.popen(command).read()  # Result is now a string.
-print("Output from command:", result.split("\n")[-2])
 
 # Reading all the lines from the file with all downloaded packages.
 with open("pip_packages.txt", "r") as f:
     packages = f.readlines()[2:]
 
-# Display each package.
-for package in packages:
-    first_space = package.index(' ')
-    curr_package = package[: first_space]
-    result = os.popen(command).read()  # Result is now a string.
-    print(curr_package)
-    print("Output from command:", result.split("\n")[-2])
+with open("non_requiredby.txt", "w+") as f:
+    # Display each package.
+    for package in packages:
+        first_space = package.index(' ')
+        curr_package = package[: first_space]
+        command = f"pip show {curr_package}"
+        result = os.popen(command).read()  # Result is now a string.
+        print(curr_package)
+        required_by = result.split("\n")[-2]
+        if len(required_by) == 13:
+            f.write(curr_package + "\n")
